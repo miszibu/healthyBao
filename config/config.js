7ac8de56f2bcd2eceb694fa0b95fc5623f5e7bab
varzibu <806768001@qq.com>
2016-06-04 11:56:07 +0800

    增加服务器端地址 和 ppt.html 演示ppt

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@
  */
 
 var mongoose = require('mongoose');//引入mongoose库
-//mongoose.connect('mongodb://localhost:27017/shuanglema');//mongodb连接地址,demo为数据库名称,默认mongodb连接不需要密码
+mongoose.connect('mongodb://localhost:27017/shuanglema');//mongodb连接地址,demo为数据库名称,默认mongodb连接不需要密码
 
-mongoose.connect('mongodb://115.28.106.162:27017/shuanglema');
+//mongoose.connect('mongodb://115.28.106.162:27017/shuanglema');//服务器端 辣鸡鸣的服务器
 
 exports.mongoose = mongoose;//导出mongoose对象
 
